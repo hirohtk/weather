0a2b85196beccf3187dcc27bea19571f415c2b7e
scotthiraki <scott.hiraki@gmail.com>
2020-05-03 18:28:10 -0700

    added bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import Animation from "./components/animation"
+import Nav from "./components/nav"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <header className="App-header">
-<Animation></Animation>
+      <header className="App-header">
+        <Nav></Nav>
+        <div className="container">
+          <div className="row">
+            <Animation></Animation>
+          </div>
+        </div>
       </header>
     </div>
   );
