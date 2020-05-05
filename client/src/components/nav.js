0bc9e4719d46d441050c81555a64ae344e408274
Kensen <hirohtk@gmail.com>
2020-05-04 20:11:21 -0700

    shifted buttons down, logo left.  moving to react dropdown instead of materialize for right side of nav

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import "./nav.css"
+import $ from 'jquery'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
+function Nav() {
 
-
-class Nav extends React.Component {
-
-
-
-    render() {
-        return (
-            
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-      </ul>
-    </div>
-  </nav>
-        )
-    }
+  const options = [
+    'one', 'two', 'three'
+  ];
+  const defaultOption = options[0];
+  
+  return (
+<>
+        {/* <ul id="dropdown1" class="dropdown-content">
+          <li><a href="#!">one</a></li>
+          <li><a href="#!">two</a></li>
+          <li class="divider"></li>
+          <li><a href="#!">three</a></li>
+        </ul> */}
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
+              <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+          </div>
+        </nav>
+      </>
+  )
 }
 
 export default Nav;
