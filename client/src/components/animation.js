2e1ad0108387785684d0befe32f40b591376d7ce
scotthiraki <scott.hiraki@gmail.com>
2020-05-06 21:05:22 -0700

    last commit before emrging with kensens

--- client/src/components/animation.js
+++ client/src/components/animation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./rain.css"
+//import "./cloud.scss"
 import $ from 'jquery'
+import _ from 'underscore'
 
 class Animation extends React.Component {
 
@@ -14,12 +16,6 @@ class Animation extends React.Component {
 
 
         var canvasEl = document.querySelector('.fireworks');
-        // var ctx = canvasEl.getContext('2d');
-        // var numberOfParticules = 30;
-        // var pointerX = 0;
-        // var pointerY = 0;
-        // var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
-        // var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
 
         function setCanvasSize() {
             canvasEl.width = window.innerWidth * 2;
@@ -29,110 +25,7 @@ class Animation extends React.Component {
             canvasEl.getContext('2d').scale(2, 2);
         }
 
-        // function updateCoords(e) {
-        //     pointerX = e.clientX || e.touches[0].clientX;
-        //     pointerY = e.clientY || e.touches[0].clientY;
-        // }
 
-        // function setParticuleDirection(p) {
-        //     var angle = anime.random(0, 360) * Math.PI / 180;
-        //     var value = anime.random(50, 180);
-        //     var radius = [-1, 1][anime.random(0, 1)] * value;
-        //     return {
-        //         x: p.x + radius * Math.cos(angle),
-        //         y: p.y + radius * Math.sin(angle)
-        //     };
-        // }
-
-        // function createParticule(x, y) {
-        //     var p = {};
-        //     p.x = x;
-        //     p.y = y;
-        //     p.color = colors[anime.random(0, colors.length - 1)];
-        //     p.radius = anime.random(16, 32);
-        //     p.endPos = setParticuleDirection(p);
-        //     p.draw = function () {
-        //         ctx.beginPath();
-        //         ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-        //         ctx.fillStyle = p.color;
-        //         ctx.fill();
-        //     };
-        //     return p;
-        // }
-
-        // function createCircle(x, y) {
-        //     var p = {};
-        //     p.x = x;
-        //     p.y = y;
-        //     p.color = '#FFF';
-        //     p.radius = 0.1;
-        //     p.alpha = 0.5;
-        //     p.lineWidth = 6;
-        //     p.draw = function () {
-        //         ctx.globalAlpha = p.alpha;
-        //         ctx.beginPath();
-        //         ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-        //         ctx.lineWidth = p.lineWidth;
-        //         ctx.strokeStyle = p.color;
-        //         ctx.stroke();
-        //         ctx.globalAlpha = 1;
-        //     };
-        //     return p;
-        // }
-
-        // function renderParticule(anim) {
-        //     for (var i = 0; i < anim.animatables.length; i++) {
-        //         anim.animatables[i].target.draw();
-        //     }
-        // }
-
-        // function animateParticules(x, y) {
-        //     var circle = createCircle(x, y);
-        //     var particules = [];
-        //     for (var i = 0; i < numberOfParticules; i++) {
-        //         particules.push(createParticule(x, y));
-        //     }
-        //     anime.timeline().add({
-        //         targets: particules,
-        //         x: function (p) { return p.endPos.x; },
-        //         y: function (p) { return p.endPos.y; },
-        //         radius: 0.1,
-        //         duration: anime.random(5500, 10000),
-        //         easing: 'easeOutExpo',
-        //         update: renderParticule
-        //     })
-        //         .add({
-        //             targets: circle,
-        //             radius: anime.random(80, 160),
-        //             lineWidth: 0,
-        //             alpha: {
-        //                 value: 0,
-        //                 easing: 'linear',
-        //                 duration: anime.random(600, 800),
-        //             },
-        //             duration: anime.random(1200, 1800),
-        //             easing: 'easeOutExpo',
-        //             update: renderParticule,
-        //             offset: 0
-        //         });
-        // }
-
-        // var render = anime({
-        //     duration: Infinity,
-        //     update: function () {
-        //         ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-        //     }
-        // });
-
-
-
-        // document.getElementById("myFireworks").addEventListener("click", function (e) {
-        //     window.human = true;
-        //     render.play();
-        //     updateCoords(e);
-        //     animateParticules(pointerX, pointerY);
-        // }, false);
-        //-------------------------------
         //RAINNNNNNNNNNNNNNNNNNNNNNN
         var centerX = window.innerWidth;
         var centerY = window.innerHeight;
@@ -161,54 +54,7 @@ class Animation extends React.Component {
             $('.rain.front-row').append(drops);
             $('.rain.back-row').append(backDrops);
         }
-        // var c = document.getElementById('myFireworks'), 
-        //     $ = c.getContext("2d");
-        // var w = c.width = window.innerWidth, 
-        //     h = c.height = window.innerHeight;
-        // function Snowy() {
-        //   var snow, arr = [];
-        //   var num = 600, tsc = 1, sp = 1;
-        //   var sc = 1.3, t = 0, mv = 20, min = 1;
-        //     for (var i = 0; i < num; ++i) {
-        //       snow = new Flake();
-        //       snow.y = Math.random() * (h + 50);
-        //       snow.x = Math.random() * w;
-        //       snow.t = Math.random() * (Math.PI * 2);
-        //       snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
-        //       snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
-        //       snow.sp = snow.sp < min ? min : snow.sp;
-        //       arr.push(snow);
-        //     }
-        //   go();
-        //   function go(){
-        //     window.requestAnimationFrame(go);
-        //       $.clearRect(0, 0, w, h);
-        //       $.fillStyle = 'hsla(242, 95%, 3%, 1)';
-        //       $.fillRect(0, 0, w, h);
-        //       $.fill();
-        //         for (var i = 0; i < arr.length; ++i) {
-        //           f = arr[i];
-        //           f.t += .05;
-        //           f.t = f.t >= Math.PI * 2 ? 0 : f.t;
-        //           f.y += f.sp;
-        //           f.x += Math.sin(f.t * tsc) * (f.sz * .3);
-        //           if (f.y > h + 50) f.y = -10 - Math.random() * mv;
-        //           if (f.x > w + mv) f.x = - mv;
-        //           if (f.x < - mv) f.x = w + mv;
-        //           f.draw();}
-        //  }
-        //  function Flake() {
-        //    this.draw = function() {
-        //       this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
-        //       this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
-        //       this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
-        //       $.moveTo(this.x, this.y);
-        //       $.fillStyle = this.g;
-        //       $.beginPath();
-        //       $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
-        //       $.fill();}
-        //   }
-        // }
+
 
         $('.splat-toggle.toggle').on('click', function () {
             $('body').toggleClass('splat-toggle');
@@ -222,11 +68,11 @@ class Animation extends React.Component {
             //makeItRain();
         });
 
-        // $('.snow-toggle.toggle').on('click', function () {
-        //     $('body').toggleClass('snow-toggle');
-        //     $('.snow-toggle.toggle').toggleClass('active');
-        //     Snowy();
-        // });
+        $('.snow-toggle.toggle').on('click', function () {
+            $('body').toggleClass('snow-toggle');
+            $('.snow-toggle.toggle').toggleClass('active');
+            init();
+        });
 
         $('.rain-toggle.toggle').on('click', function () {
             console.log("button clicked")
@@ -237,64 +83,112 @@ class Animation extends React.Component {
 
         //----------------------------------------------------
         //SNOWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
-        // var c = document.getElementById('myFireworks'), 
-        //     $ = c.getContext("2d");
-        // var w = c.width = window.innerWidth, 
-        //     h = c.height = window.innerHeight;
+        var canvas = document.querySelector('.fireworks'),
+            ctx = canvas.getContext('2d'),
+            windowW = window.innerWidth,
+            windowH = window.innerHeight,
+            numFlakes = 200,
+            flakes = [];
 
-        // Snowy();
-        // function Snowy() {
-        //   var snow, arr = [];
-        //   var num = 600, tsc = 1, sp = 1;
-        //   var sc = 1.3, t = 0, mv = 20, min = 1;
-        //     for (var i = 0; i < num; ++i) {
-        //       snow = new Flake();
-        //       snow.y = Math.random() * (h + 50);
-        //       snow.x = Math.random() * w;
-        //       snow.t = Math.random() * (Math.PI * 2);
-        //       snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
-        //       snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
-        //       snow.sp = snow.sp < min ? min : snow.sp;
-        //       arr.push(snow);
-        //     }
-        //   go();
-        //   function go(){
-        //     window.requestAnimationFrame(go);
-        //       $.clearRect(0, 0, w, h);
-        //       $.fillStyle = 'hsla(242, 95%, 3%, 1)';
-        //       $.fillRect(0, 0, w, h);
-        //       $.fill();
-        //         for (var i = 0; i < arr.length; ++i) {
-        //           f = arr[i];
-        //           f.t += .05;
-        //           f.t = f.t >= Math.PI * 2 ? 0 : f.t;
-        //           f.y += f.sp;
-        //           f.x += Math.sin(f.t * tsc) * (f.sz * .3);
-        //           if (f.y > h + 50) f.y = -10 - Math.random() * mv;
-        //           if (f.x > w + mv) f.x = - mv;
-        //           if (f.x < - mv) f.x = w + mv;
-        //           f.draw();}
-        //  }
-        //  function Flake() {
-        //    this.draw = function() {
-        //       this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
-        //       this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
-        //       this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
-        //       $.moveTo(this.x, this.y);
-        //       $.fillStyle = this.g;
-        //       $.beginPath();
-        //       $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
-        //       $.fill();}
-        //   }
-        // }
+        function Flake(x, y) {
+            var maxWeight = 5,
+                maxSpeed = 3;
+
+            this.x = x;
+            this.y = y;
+            this.r = randomBetween(0, 1);
+            this.a = randomBetween(0, Math.PI);
+            this.aStep = 0.01;
 
 
+            this.weight = randomBetween(2, maxWeight);
+            this.alpha = (this.weight / maxWeight);
+            this.speed = (this.weight / maxWeight) * maxSpeed;
+
+            this.update = function () {
+                this.x += Math.cos(this.a) * this.r;
+                this.a += this.aStep;
+
+                this.y += this.speed;
+            }
+
+        }
+
+        function init() {
+            var i = numFlakes,
+                flake,
+                x,
+                y;
+
+            while (i--) {
+                x = randomBetween(0, windowW, true);
+                y = randomBetween(0, windowH, true);
 
 
+                flake = new Flake(x, y);
+                flakes.push(flake);
+            }
+
+            scaleCanvas();
+            loop();
+        }
+
+        function scaleCanvas() {
+            canvas.width = windowW;
+            canvas.height = windowH;
+        }
+
+        function loop() {
+            var i = flakes.length,
+                z,
+                dist,
+                flakeA,
+                flakeB;
+
+            // clear canvas
+            ctx.save();
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+            ctx.clearRect(0, 0, windowW, windowH);
+            ctx.restore();
+
+            // loop of hell
+            while (i--) {
+
+                flakeA = flakes[i];
+                flakeA.update();
 
 
+                ctx.beginPath();
+                ctx.arc(flakeA.x, flakeA.y, flakeA.weight, 0, 2 * Math.PI, false);
+                ctx.fillStyle = 'rgba(255, 255, 255, ' + flakeA.alpha + ')';
+                ctx.fill();
 
+                if (flakeA.y >= windowH) {
+                    flakeA.y = -flakeA.weight;
+                }
+            }
 
+            requestAnimationFrame(loop);
+        }
+
+        function randomBetween(min, max, round) {
+            var num = Math.random() * (max - min + 1) + min;
+
+            if (round) {
+                return Math.floor(num);
+            } else {
+                return num;
+            }
+        }
+
+        function distanceBetween(vector1, vector2) {
+            var dx = vector2.x - vector1.x,
+                dy = vector2.y - vector1.y;
+
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+
+        //init();
 
         //makeItRain();
         setCanvasSize();
