c77809be7fc3b07ce7c56b7f2a0eef9bbf1c3274
scotthiraki <scott.hiraki@gmail.com>
2020-06-10 19:46:31 -0700

    not sure what i change

--- client/src/components/currentWeather.js
+++ client/src/components/currentWeather.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import "./currentWeather.css"
 import { iconLogic } from "./logic/iconLogic";
+import moment from "moment";
+
 
 function CurrentWeather(props) {
 
     return (
         <>
-
-
             <div className="outerCurrentWeatherBox">
                 <div className="weather-side">
                     <div className="weather-gradient"></div>
-                        {/* <h2 className="date-dayname">Tuesday</h2> */}
-                        {/* <span class="date-day">15 Jan 2019</span> */}
-                        <h5 className="testText">{props.location}</h5>
-                        {
-                            props.weather[1] === undefined ? <></> : <span>{iconLogic(props.weather[1])}</span>
-                        }
-                        <p className="whiteTextInfo">{props.weather[0]} &#xb0;F</p>
-                        <p className="whiteTextInfo">{props.weather[1]}</p>
+                    {/* <h2 className="date-dayname">Tuesday</h2> */}
+                    {/* <span class="date-day">15 Jan 2019</span> */}
+                    <h5 className="testText">{props.location}</h5>
+                    {
+                        props.weather[1] === undefined ? <></> : <span>{iconLogic(props.weather[1])}</span>
+                    }
+                    <p className="whiteTextInfo">{props.weather[0]} &#xb0;F</p>
+                    <p className="whiteTextInfo">{props.weather[1]}</p>
 
-                    </div>
                 </div>
-            
+            </div>
+
             {/* <div className="outerCurrentWeatherBox">
                 <h5 className="testText">Current Weather for {props.now}</h5>
                 <div className="currentWeatherBox">
