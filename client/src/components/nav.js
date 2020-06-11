98bc743ca283966cf46edd5e5a02e49d4d95eb1b
Kensen <hirohtk@gmail.com>
2020-06-10 21:20:21 -0700

    got rid of nav, made line stroke width and color chnaged

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -28,40 +28,27 @@ class Nav extends React.Component {
     const props = this.props
     return (
       <>
-        {/* <ul id="dropdown1" class="dropdown-content">
-              <li><a href="#!">one</a></li>
-              <li><a href="#!">two</a></li>
-              <li class="divider"></li>
-              <li><a href="#!">three</a></li>
-            </ul> */}
-        <nav>
         <Sidebar
-              sidebar={<><b>Sidebar content</b>
-              <hr></hr>
-              <div>1</div>
-              <div>2</div>
-              <div>3</div>
-              <div>4</div>
-
-              </>}
-              open={this.state.sidebarOpen}
-              onSetOpen={this.onSetSidebarOpen}
-              styles={{ sidebar: { background: "gray", width: "20rem" } }}
-            >
-              
-            </Sidebar>
+          sidebar={<><b>Settings</b>
+            <hr></hr>
+            <div>Login</div>
+            <div>Sign Up</div>
+          </>}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: "gray", width: "20rem", position: "fixed" } }}
+        >
+        </Sidebar>
+        <button className="sidebarButton" onClick={() => this.onSetSidebarOpen(true)}>
+              <i class="material-icons">menu</i>
+            </button>
+        {/* <nav>
           <div class="nav-wrapper">
-          <button className="sidebarButton" onClick={() => this.onSetSidebarOpen(true)}>
-          <i class="material-icons">menu</i>
-        </button>
             <a href="#" class="brand-logo">Weather App</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-              {/* <li id="time">{this.state.now}</li> */}
-              {/* <li><Dropdown options={options} onChange={_onSelect} value={"|||"} placeholder="Select an option" /></li> */}
-              {/* <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li> */}
             </ul>
           </div>
-        </nav>
+        </nav> */}
       </>
     )
   }
