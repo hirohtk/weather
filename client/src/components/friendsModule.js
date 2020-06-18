5b8d84438163a5ac7fa5e6af65ff11e2254de695
Kensen <hirohtk@gmail.com>
2020-06-17 20:00:44 -0700

    have results from db query for username rendered with clear function.  should be scalable for finding multiple results later, as these results are being mapped

--- client/src/components/friendsModule.js
+++ client/src/components/friendsModule.js
@@ -7,15 +7,26 @@ class Friends extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchTerm: ""
+            searchTerm: "",
+            friendResults: [],
+            searching: false
         }
     }
 
-    searchInputHandler = (event) => this.setState({searchTerm: event.target.value})
+    searchInputHandler = (event) => this.setState({ searchTerm: event.target.value })
+
+    clearResults = () => this.setState({ friendResults: [], searching: false });
 
     searchHandler = () => {
         axios.get(`/api/allusers/${this.state.searchTerm}`).then(response => {
             console.log(response.data);
+            if (response.data.length === 0) {
+                this.setState({ friendResults: [] });
+            }
+            else {
+                this.setState({ friendResults: [response.data[0].username] });
+            }
+            this.setState({ searching: true });
         })
     }
 
@@ -30,8 +41,19 @@ class Friends extends React.Component {
                         <p className="theFriends"><i class="material-icons online">lens</i>Friend 2 <img className="tinyFriendPic" src="https://cultofthepartyparrot.com/parrots/hd/partyparrot.gif"></img></p>
                         <p className="theFriends"><i class="material-icons online">lens</i>Friend 3 <img className="tinyFriendPic" src="https://cultofthepartyparrot.com/parrots/hd/shuffleparrot.gif"></img></p>
                         <div className="searchForFriends">
-                        <input placeholder="Search for a user!" name="searchTerm" value={this.state.searchTerm} maxLength="16" onChange={this.searchInputHandler}></input>
-                        <button id="loginSubmit" onClick={this.searchHandler}>Go!</button>
+                            <input placeholder="Search for a user!" name="searchTerm" value={this.state.searchTerm} maxLength="16" onChange={this.searchInputHandler} className="whiteText"></input>
+                            <button id="loginSubmit" onClick={this.searchHandler}>Go!</button>
+                            {this.state.searching ?
+                                <>{this.state.friendResults.length === 0 ?
+                                    <><h5 className="whiteText">Search Results</h5>
+                                        <p className="whiteText">None, try again!</p>
+                                    </> :
+                                    <> <h5 className="whiteText">Search Results</h5>
+                                        {this.state.friendResults.map((each, index) =>
+                                            <p className="whiteText">{each}</p>)}
+                                        <button onClick={this.clearResults}>Clear</button>
+                                    </>}
+                                </> : <></>}
                         </div>
                     </div>
 
