af1ccb8153dee77189387248ec2a636854942574
Kensen <hirohtk@gmail.com>
2020-07-27 21:27:06 -0700

    fixed redunant socket close in friendsmodule which was creating more socket issues.  have additional socket issues when users logout though

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
       friendCurrentWeather: [],
     }
     // console.log(`console logging inside the constructor of App.js`);
-    this.socket = io('https://immense-cove-75264.herokuapp.com/' || 'localhost:3001');
+    this.socket = io('localhost:3001');
 
   }
 
--- socket/sockets.js
+++ socket/sockets.js
@@ -8,11 +8,11 @@ module.exports = function (io) {
 
         socket.on("join", async (room, test) => {
             // roomid = room;
-            console.log(`THIS IS SOCKET ${socket.id} WHO IS NOW JOINING ROOM ${room}, and has a mongoID of ${test.id}`)
+            // console.log(`THIS IS SOCKET ${socket.id} WHO IS NOW JOINING ROOM ${room}, and has a mongoID of ${test.id}`)
             socket.join(room);
             // from https://stackoverflow.com/questions/50045613/how-to-find-all-sockets-in-room-using-the-latest-version-of-socket-io
             let connecteds = io.sockets.adapter.rooms[room].sockets;
-            console.log(`connecteds are ${JSON.stringify(connecteds)}. *** These are the people who are connected to the same room, ${room}.`)
+            console.log(`room joined.  connecteds are ${JSON.stringify(connecteds)}. *** These are the people who are connected to the same room, ${room}.`)
             let obj = {room: room, who: test.id, username: socket.id, connected: connecteds};
             io.emit("roomJoined", obj);
         });
@@ -34,13 +34,13 @@ module.exports = function (io) {
             // COULD DO POPULATE HERE INSTEAD I THINK TO GET USERNAME FROM author
             const userName = await db.Users.findById(author);
             chatMessage.author = userName.username;
-            console.log(`Updating chatroom ${chatRoomID}, pushing message with id of ${chatMessage._id}`)
+            // console.log(`Updating chatroom ${chatRoomID}, pushing message with id of ${chatMessage._id}`)
             db.Chatroom.findByIdAndUpdate(chatRoomID, { $push: { messages: chatMessage._id } }).then(response => {
                 console.log(`response from adding message to chatroom is ${response}.`)
-                console.log(`JSON.stringifying is ${JSON.stringify(response)}.`)
+                // console.log(`JSON.stringifying is ${JSON.stringify(response)}.`)
             })
             
-            console.log(`pushed message to chatroom ID ${chatRoomID}, message id is ${chatMessage._id}, author is ${chatMessage.author}, message is ${message}`)
+            // console.log(`pushed message to chatroom ID ${chatRoomID}, message id is ${chatMessage._id}, author is ${chatMessage.author}, message is ${message}`)
 
             // adding username key to author because the db response convention is as such, so for continuity in the .map, I am doing this
             const newObj = { message: chatMessage.message, author: {username: userName.username}, chatroomID: chatRoomID };
